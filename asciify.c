3c0ad13839bc8939a244813a8dc0c7e9703a5505
hoshiko <okinawa91now@gmail.com>
2025-02-01 04:59:31 +0300

    0.1(release)

---
+++ asciify.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encrypt(const char* str) {
+	for (int i = 0; i < strlen(str); ++i) {
+		printf("%d", str[i]);
+	}
+}
+
+void decrypt(char* str) {
+	int last = 0;
+	while (strlen(str) > 1) {
+		char tmp_str[strlen(str)];
+		strcpy(tmp_str, str);
+
+		if (strlen(tmp_str) > 2) {
+			tmp_str[3] = '\0';
+			int tmp_num = atoi(tmp_str);
+
+			if (tmp_num < 127) {
+				printf("%c", tmp_num);
+				memmove(str, str + 3, strlen(str) - 2);
+				continue;
+			}
+			strcpy(tmp_str, str);
+		}
+
+		tmp_str[2] = '\0';
+		int tmp_num = atoi(tmp_str);
+		
+		if (tmp_num > 31) {
+			printf("%c", tmp_num);
+		} else {
+			printf("(null)");
+		}
+		memmove(str, str + 2, strlen(str) - 1);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		printf("Usage: asciify [OPTION] [MESSAGE]\n  -e\tencrypts the input\n  -d\tdecrypts the input");
+		exit(1);
+	}
+
+	if (strcmp(argv[1], "-e") == 0) {
+		printf("Encrypted: ");
+		encrypt(argv[2]);
+		printf("\n");
+	} else if (strcmp(argv[1], "-d") == 0) {
+                printf("Decrypted: ");
+                decrypt(argv[2]);
+                printf("\n");
+	} else {
+		printf("Usage: asciify [OPTION] [MESSAGE]\n  -e\tencrypts the input\n  -d\tdecrypts the input");
+		exit(1);
+	}
+
+
+	return 0;
+}
